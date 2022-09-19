26985b4870755acd0cc283cf92d711f821c0ba90
david660 <daodu.muyiwa@gmail.com>
2022-09-19 21:14:20 +0100

    Remove images overlay

--- components/Image.js
+++ components/Image.js
@@ -1,22 +1,11 @@
 import Image from "next/image"
-import React, {useState} from "react"
-import styles from '../styles/Image.module.css'
+import React from "react"
 
 const Img = ({src, alt, className}) =>{
-    const [show, setShow] = useState(false)
-    
-    const mouseOver = () =>{
-        setShow(true)
-    }
-
-    const mouseLeave = () =>{
-        setShow(false)
-    }
 
     return (
-        <div className={`relative w-full overflow-hidden p-0 text-[0px] ${className}`} onMouseOver={mouseOver} onMouseLeave={mouseLeave}>
-            <Image src={src} alt={alt} className={`mx-auto ${!show && 'grayscale'} p-0`}/>
-            <div className={`${styles.imgOverlay} absolute left-0 w-full h-full backdrop-opacity-40 backdrop-grayscale bg-white/10 grayscale ${show ? '-top-full' : 'top-0'}`}></div>
+        <div className={`relative w-full overflow-hidden p-0 text-[0px] ${className}`}>
+            <Image src={src} alt={alt} className={`mx-auto p-0`}/>
         </div>
     )
 }
